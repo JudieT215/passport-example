01fd5a24cc3192820add4487695bb14de2eb85d9
okputadora <okputadora@gmail.com>
2018-06-21 19:18:54 -0400

    added authentication function

--- routes/passport.js
+++ routes/passport.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const Router = express.Router();
-
+const passport = require('passport');
+const User = require('./models/passport');
 Router.post('/login', (req, res, next) => {
 
 });
+
+Router.post('/register', (req, res, next) => {
+  User.register(new Account({username: req.body.username}), req.body.paddword, (err, user) => {
+    if (err) {
+      return res.render('error', {error: err})
+    }
+    passport.authenticate('local')(req, res, next) {
+      res.redirect('/');
+    }
+  } )
+})
 
 module.exports = Router;
