2003ab8ce01b8a8d7215fd53d5df88f01bafffe5
okputadora <okputadora@gmail.com>
2018-06-21 18:58:02 -0400

    added model to server

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
 const app = express();
 const PORT = process.envPORT || 3001;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
+
+const User = require('./models/user');
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/authentication');
 
 app.list(PORT, () => {
